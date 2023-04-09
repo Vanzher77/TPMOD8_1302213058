a8162633dae7b980f65fac1b8d8bd96a37e77d11
Vanzher77 <121426519+Vanzher77@users.noreply.github.com>
2023-04-10 01:19:07 +0700

    menyelesaikan program dengan runtime configuration

--- TP_MOD8_1302213058/TP_MOD8_1302213058/CovidConfig.cs
+++ TP_MOD8_1302213058/TP_MOD8_1302213058/CovidConfig.cs
@@ -1,8 +1,85 @@
 ﻿using System;
+using System.Text.Json;
 
-public class Class1
+public class Covid
 {
-	public Class1()
-	{
-	}
+	public string satuan_suhu {get; set;}
+    public int batas_hari_demam { get; set; }
+    public string pesan_ditolak { get; set; }
+    public string pesan_diterima { get; set; }
+
+    public Covid(string suhu, int demam, string ditolak, string diterima)
+    {
+        this.satuan_suhu = suhu;
+        this.batas_hari_demam = demam;
+        this.pesan_ditolak = ditolak;
+        this.pesan_diterima = diterima;
+    }
+    public Covid() { }
+}
+
+
+
+public class config
+{
+    //membuat path dan file location untuk membaca file json berada dimana
+    public const string fileLocation = "E:\\DATA KULIAH\\SEMESTER 4\\Konstruksi Perangkat Lunak (KPL)\\TP Mod 8\\TP_MOD8_1302213058\\TP_MOD8_1302213058\\bin\\Debug\\net6.0";
+    public const string filePath = fileLocation + @"CovidConfig.json";
+    
+    public Covid covid;
+    
+    public config() 
+    {
+        //menggunakan try catch block untuk menangkap dan menangani error yang di hasilkan oleh kode
+        try
+        {
+           readJSON();
+        }
+        catch 
+        {
+            defaults();
+            write();
+            
+            
+        }
+    }
+    
+    //untuk mengset defaults file json
+    public void defaults()
+    {
+        covid = new Covid("Celcius", 14, "Anda tidak diperbolehkan masuk ke dalam gedung ini", "Anda dipersilahkan masuk ke dalam gedung ini");
+    }
+
+    //untuk menulis file konfigurasi dari json
+    public void write()
+    {
+        var wrt = new JsonSerializerOptions { WriteIndented = true };
+        string str = JsonSerializer.Serialize(covid, wrt);
+        File.WriteAllText(@"CovidConfig.json", str);
+        
+    }
+    //mengset filepath json
+    private Covid readJSON()
+    {
+        
+        string hasilbaca = File.ReadAllText(filePath);
+        covid = JsonSerializer.Deserialize<Covid>(hasilbaca);
+        return covid;
+    }
+    //mengubah satuan celcius dan fahrenheit yang akan keluar di output
+    public void UbahSatuan()
+    {
+        if(covid.satuan_suhu == "Celcius")
+        {
+            covid.satuan_suhu = "fahrenheit";
+            
+            
+        }
+        else
+        {
+            covid.satuan_suhu = "Celcius";
+        }
+        
+        
+    }
 }
--- TP_MOD8_1302213058/TP_MOD8_1302213058/Program.cs
+++ TP_MOD8_1302213058/TP_MOD8_1302213058/Program.cs
@@ -1,1 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+
+public class program
+{
+ 
+    private static void Main(string[] args)
+    {
+        config Config = new config();
+        Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai " + Config.covid.satuan_suhu);
+        //menggunakan tipe data double dikarenakan menginput celcius 
+        double suhu = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        //menggunakan tipe data integer untuk menginput hari
+        int demam = Convert.ToInt32(Console.ReadLine());
+        //menggunakan if untuk memeriksa suatu kondisi dan mengeksekusi kode tertentu. 
+        if(Config.covid.satuan_suhu == "Celcius")
+        {
+            string text = suhu >= 36.5 && suhu <= 37.5 && demam < Config.covid.batas_hari_demam ? Config.covid.pesan_diterima : Config.covid.pesan_ditolak;
+            Console.WriteLine(text);
+        }
+        else if (Config.covid.satuan_suhu == "fahrenheit")
+        {
+            string text = suhu >= 97.7 && suhu <= 99.5 && demam < Config.covid.batas_hari_demam ? Config.covid.pesan_diterima : Config.covid.pesan_ditolak;
+            Console.WriteLine(text);
+        }
+        Console.WriteLine();
+        //mengubah celcius menjadi fahrenheit
+        Config.UbahSatuan();
+        Console.WriteLine("Berapa suhu badan anda saat ini ? Dalam nilai " + Config.covid.satuan_suhu);
+        suhu = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        demam = Convert.ToInt32(Console.ReadLine());
+        //menggunakan if untuk memeriksa suatu kondisi dan mengeksekusi kode tertentu. 
+        if (Config.covid.satuan_suhu == "Celcius")
+        {
+            string text = suhu >= 36.5 && suhu <= 37.5 && demam < Config.covid.batas_hari_demam ? Config.covid.pesan_diterima : Config.covid.pesan_ditolak;
+            Console.WriteLine(text);
+        }
+        else if (Config.covid.satuan_suhu == "fahrenheit")
+        {
+            string text = suhu >= 97.7 && suhu <= 99.5 && demam < Config.covid.batas_hari_demam ? Config.covid.pesan_diterima : Config.covid.pesan_ditolak;
+            Console.WriteLine(text);
+            //menggunakan console readline pada akhir console writeline, dikarenakan jika tidak memakai console readline terjadi error
+            //error disini saat kita menginputkan suatu kondisi akan terjadi force close pada output nya
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
